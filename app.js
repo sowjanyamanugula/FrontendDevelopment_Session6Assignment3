0d903927c02abcf5a22490d024adf4e73c96beee
sowjanyamanugula <manugulasowjanya@gmail.com>
2017-01-11 12:52:28 +0530

    Frontend Devlopment Session6 Assignment3 code
    Frontend Devlopment Session6 Assignment3 code

---
+++ app.js
@@ -0,0 +1,15 @@
+var sum = 0;
+for (var i = 1; i <= 1000; i++)
+{
+		sum = sum + i;
+}
+console.log("For loop Sum = " + sum);
+
+ var sum = 0;
+var j=1;
+ while(j<=1000)
+{
+       sum = sum + j;
+       j+=1;
+ }
+ console.log("While loop Sum = " + sum);           
